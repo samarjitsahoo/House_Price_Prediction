0613b2d33c665c702e019ee85cd2cff66b243ad4
Selmon Sahoo <samarjit9203@gmail.com>
2024-04-05 14:10:26 +0530

    Updated

--- main.py
+++ main.py
@@ -7,6 +7,25 @@ model = load('Dragon.joblib')
 
 # Define the input fields for user input
 st.title('Selmon Real Estate - Price Predictor')
+st.write('''Attribute Information:
+
+    1. CRIM      per capita crime rate by town
+    2. ZN        proportion of residential land zoned for lots over 
+                 25,000 sq.ft.
+    3. INDUS     proportion of non-retail business acres per town
+    4. CHAS      Charles River dummy variable (= 1 if tract bounds 
+                 river; 0 otherwise)
+    5. NOX       nitric oxides concentration (parts per 10 million)
+    6. RM        average number of rooms per dwelling
+    7. AGE       proportion of owner-occupied units built prior to 1940
+    8. DIS       weighted distances to five Boston employment centres
+    9. RAD       index of accessibility to radial highways
+    10. TAX      full-value property-tax rate per $10,000
+    11. PTRATIO  pupil-teacher ratio by town
+    12. B        1000(Bk - 0.63)^2 where Bk is the proportion of blacks 
+                 by town
+    13. LSTAT    % lower status of the population
+    14. MEDV     Median value of owner-occupied homes in $1000's''')
 st.write('Enter the following details to get the predicted house price:')
 
 CRIM = st.number_input('CRIM (per capita crime rate by town)')
